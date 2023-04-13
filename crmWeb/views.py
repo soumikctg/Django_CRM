1f69afe2289efc9d397625206e8a9e7ca898c2d4
Soumik Dutta <soumikctg616@gmail.com>
2023-04-13 13:23:25 +0600

    4th Commit

--- crmWeb/views.py
+++ crmWeb/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+
 # Create your views here.
 def home(request):
     if request.method == 'POST':
-        username = request.POST['user_name']
+        username = request.POST['username']
         password = request.POST['password']
         #authenticate
         
@@ -22,7 +24,22 @@ def home(request):
         return render(request, 'home.html', {})
 
 def registerUser(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+			# Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+
+    return render(request, 'register.html', {'form':form})
    
 
 def logoutUser(request):
